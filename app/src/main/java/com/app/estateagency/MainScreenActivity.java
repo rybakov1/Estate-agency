3a0c7e01826e844a8635294825ad3c25903833e9
Владислав <rybvlad4@yandex.ru>
2022-11-29 15:42:48 +0300

    add MainScreenActivity

--- app/src/main/java/com/app/estateagency/MainScreenActivity.java
+++ app/src/main/java/com/app/estateagency/MainScreenActivity.java
@@ -6,30 +6,23 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.app.estateagency.fragments.PlateListFragment;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainScreenActivity extends AppCompatActivity {
 
-    List<Apartment> aparts = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
-        setInitialData();
-
-        RecyclerView recyclerView = findViewById(R.id.plates_list);
-        ApartmentAdapter adapter = new ApartmentAdapter(aparts);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-    }
-
-    private void setInitialData() {
-        int[] images = {R.drawable.ic_launcher_background, R.drawable.ic_launcher_background};
-        aparts.add(new Apartment(images, 1230000, "wow", "м. Фрунзенская"));
-        aparts.add(new Apartment(images, 40000, "wow", "м. Московская"));
-        aparts.add(new Apartment(images, 1000, "wow", "м. ЗЕмлянная"));
+        PlateListFragment plateListFragment = PlateListFragment.newInstance();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.main_fragment, plateListFragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
